4b77b95b5b0bf3f20ae0f8965def85144a676a36
brunobincoletto <brunoggmb@gmail.com>
2011-08-10 16:06:31 -0300

    dropdown

--- holy/src/main/webapp/holy/component/dropdown/dropdown.js
+++ holy/src/main/webapp/holy/component/dropdown/dropdown.js
@@ -1,7 +1,9 @@
-$('.dropdown li').hover(function(){
-	$(this).children('ul').slideToggle();
-	$(this).addClass('hover');
-},function(){
-	$(this).children('ul').fadeOut();
-	$(this).removeClass('hover');
-});
+jQuery.fn.dropdown = function () {
+    $(this).children('li').hover(function(){
+		$(this).children('ul').slideToggle();
+		$(this).addClass('hover');
+	},function(){
+		$(this).children('ul').fadeOut();
+		$(this).removeClass('hover');
+	});
+}
