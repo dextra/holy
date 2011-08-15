65c6c7a4ddf257e7323dcf731b43b04f68a984c2
brunobincoletto <brunoggmb@gmail.com>
2011-08-15 17:38:37 -0300

    flexigrid, layout

--- holy/src/main/webapp/holy/component/dropdown/dropdown.js
+++ holy/src/main/webapp/holy/component/dropdown/dropdown.js
@@ -1,12 +1,7 @@
 (function($) {
 	$(document).ready(function(){
-		$('.dropdown > li, .dropdown ul > li').live('hover', function(){
+		$('.dropdown').delegate('li', 'hover', function(){
 			$(this).children('ul').slideToggle('fast');
-			$(this).addClass('hover');
-		}, function(){
-			$(this).children('ul').fadeOut('fast');
-			$(this).removeClass('hover');	
-		});
-	
+		});	
 	});
  })(jQuery);
--- holy/src/main/webapp/holy/themes/liquidblue/events.js
+++ holy/src/main/webapp/holy/themes/liquidblue/events.js
@@ -1,5 +1,5 @@
 (function($) {
-
-
-
+	$(document).ready(function(){
+		$('#content').corner();
+	});
 })(jQuery);
