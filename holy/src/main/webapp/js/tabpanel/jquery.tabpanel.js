bee8c0300e04770efaa8ba833ab396cfc2259707
murer <fuweweu@gmail.com>
2011-10-10 17:27:56 -0300

    active

--- holy/src/main/webapp/js/tabpanel/jquery.tabpanel.js
+++ holy/src/main/webapp/js/tabpanel/jquery.tabpanel.js
@@ -1,4 +1,7 @@
 (function($) {
+	$.fn.tabPanelShow = function(index) {
+		$(this).find('> ul > li').eq(index).find('a').click();
+	}
 	$.fn.tabPanel = function(opts) {
 		$(this).addClass('tabPanel');
 		opts = opts || {};
@@ -7,8 +10,18 @@
 
 		var tabpanel = $(this);
 		tabpanel.children("div:gt(0)").hide();
+		
+		tabpanel.bind('open.tabPanel', function(evt, index) {
+			var lis = tabpanel.children('ul').children('li');
+			var divs = tabpanel.children('div');
+			lis.removeClass('active');
+			divs.removeClass('active');
+			lis.eq(index).addClass('active');
+			divs.eq(index).addClass('active');
+			return false;
+		});
 
-		tabpanel.find("ul li a").click(function() {
+		tabpanel.find("> ul > li a").click(function() {
 			var me = $(this);
 			var parent = me.parents("div:first");
 			var index = me.closest("li").index();
@@ -23,5 +36,9 @@
 			}
 			parent.trigger("open.tabPanel", index);
 		});
+		
+		if(tabpanel.find('> ul > li').length) {
+			tabpanel.tabPanelShow(0);
+		}
 	}
 }(jQuery));
--- holy/src/main/webapp/js/togglepanel/jquery.togglepanel.js
+++ holy/src/main/webapp/js/togglepanel/jquery.togglepanel.js
@@ -1,17 +1,36 @@
 ( function($) {
+	
+	$.fn.tooglePanelShow = function() {
+		var opts = $(this).data('tooglepanel.opts');
+		opts = $.extend({}, opts);
+		var effect = opts.effect || 'slideToggle';
+		var delay = opts.delay;
+		var content = $(this).children(opts.content || 'div');
+		var trigger = function() {
+			$(this).parent().trigger("toggle.togglePanel");
+		} 
+		if(delay) {
+			content[effect](delay, trigger);
+		} else {
+			content[effect](trigger);
+		}
+	}
+	
 	$.fn.togglePanel = function(opts) {
 		$(this).addClass('togglePanel');
 		opts = opts || {};
+		$(this).data('tooglepanel.opts', opts);
 		var title = $(this).children(opts.title || 'h3');
-		var effect = opts.effect || 'slideToggle';
-		var delay = opts.delay;
 		title.click(function() {
-			var content = $(this).parent().children(opts.content || 'div');
-			if(delay) {
-				content[effect](delay);
+			$(this).parent().tooglePanelShow();
+		});
+		$(this).bind('toggle.togglePanel', function() {
+			if($(this).children('div:visible').length) {
+				$(this).addClass('active');
 			} else {
-				content[effect]();
+				$(this).removeClass('active');
 			}
 		});
+		$(this).trigger("toggle.togglePanel");
 	}
 }(jQuery));
