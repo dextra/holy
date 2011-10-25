5005a83977a782e6c1a38e5190a1628368945659
brunobincoletto <brunoggmb@gmail.com>
2011-10-25 16:17:04 -0200

    dialog

--- holy/src/main/webapp/js/dialog/jquery.dialog.js
+++ holy/src/main/webapp/js/dialog/jquery.dialog.js
@@ -1,46 +1,54 @@
 (function($) {
 	jQuery.fn.xdialog = function(opts) {
-		
 		opts = $.extend({}, opts);
-		
-		// dialog options \o/
-		
+
 		var overColor = opts.overlay;
 		if(!overColor) {
-			overColor = '#000000'
+			overColor = 'none'
 		}
-		
 		var alpha = opts.alpha;
 		if(!alpha) {
-			alpha = '7'
+			alpha = '5'
+		}
+		var dialogWidth = opts.width;
+		if(!dialogWidth) {
+			dialogWidth = 'auto'
 		}
 		
-		// if overlay is on
-		$('body').append('<div class="dOverlay" style="background:'+ overColor +'; opacity:0.'+ alpha +'; filter: alpha(opacity = '+ alpha +'0);"></div>');
-		
-		// VARS =)
+		$('body').append('<div class="dOverlay" style="background:' + overColor + '; opacity:0.' + alpha + '; filter: alpha(opacity = ' + alpha + '0);"></div>');
+
 		var me = $(this)
-		
+
 		var close = '<a href="#" class="close">(x)</a>'
-		
-		
+
 		$(this).appendTo('body');
-		
-		me.prepend(close); // add close button on dialog box.
-		
-		me.addClass('dialogBox'); // add class to identify dialog box style
-		
-		me.center(); // centralize dialog box
-		
+
+		me.prepend(close);
+
+		me.addClass('dialogBox');
+
+		me.css('width', dialogWidth);
+
+		me.center();
+
 		me.fadeIn();
-		
-		
-		$(".dOverlay, .dialogBox .close").click(function() {
-			$('.dOverlay').fadeOut();
-			$('.dialogBox').fadeOut();
-			$('.dialogBox .close').remove();
+
+		me.prev('div.dOverlay').click(function() {
+			$(this).find(' + .dialogBox a.close').click();
+		});
+
+		me.find('.close').click(function() {
+			var me = $(this);
+			var popup = me.closest('.dialogBox');
+			var overlay = popup.prev('div.dOverlay');
+			me.remove();
+			if(popup.length) {
+				popup.fadeOut('fast');
+			}
+			if(overlay.length) {
+				overlay.remove();
+			}
 			return false;
 		});
-		
 	}
 })(jQuery);
