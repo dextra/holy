b9cddd51925f4817f486ee84571e829c90371d54
murer <fuweweu@gmail.com>
2011-10-28 09:13:53 -0200

    undialog

--- holy/src/main/webapp/js/dialog/jquery.dialog.js
+++ holy/src/main/webapp/js/dialog/jquery.dialog.js
@@ -1,21 +1,24 @@
 (function($) {
-	jQuery.fn.xdialog = function(opts) {
+	$.fn.xdialog = function(opts) {
 		opts = $.extend({}, opts);
 
 		var overColor = opts.overlay;
-		if(!overColor) {
+		if (!overColor) {
 			overColor = 'none'
 		}
 		var alpha = opts.alpha;
-		if(!alpha) {
+		if (!alpha) {
 			alpha = '5'
 		}
 		var dialogWidth = opts.width;
-		if(!dialogWidth) {
+		if (!dialogWidth) {
 			dialogWidth = 'auto'
 		}
-		
-		$('body').append('<div class="dOverlay" style="background:' + overColor + '; opacity:0.' + alpha + '; filter: alpha(opacity = ' + alpha + '0);"></div>');
+
+		$('body').append(
+				'<div class="dOverlay" style="background:' + overColor
+						+ '; opacity:0.' + alpha + '; filter: alpha(opacity = '
+						+ alpha + '0);"></div>');
 
 		var me = $(this)
 
@@ -33,25 +36,27 @@
 		me.center();
 
 		me.fadeIn();
-		
+
 		me.prev('div.dOverlay').click(function() {
 			$(this).find(' + .dialogBox a.close').click();
 		});
 
-		me.find('.close').click(function() {
-			var me = $(this);
-			var popup = me.closest('.dialogBox');
-			var overlay = popup.prev('div.dOverlay');
-			me.remove();
-			if(popup.length) {
-				popup.hide();
-				popup.data('xdialog.parent').append(popup);
-			}
-			if(overlay.length) {
-				overlay.remove();
-			}
-			return false;
-		});
-		
+		me.find('a.close:first').click($.fn.xundialog);
 	}
+
+	$.fn.xundialog = function() {
+		var popup = $(this).closest('.dialogBox');
+		var a = popup.children('a.close:first');
+		var overlay = popup.prev('div.dOverlay');
+		a.remove();
+		if (popup.length) {
+			popup.hide();
+			popup.data('xdialog.parent').append(popup);
+		}
+		if (overlay.length) {
+			overlay.remove();
+		}
+		return false;
+	}
+
 })(jQuery);
