8e663c38a9b6a693ec0c755144bf41da67b35121
brunobincoletto <brunoggmb@gmail.com>
2011-12-01 14:56:42 -0200

    dextra theme

---
+++ src/main/webapp/themes/dextra/events.js
@@ -0,0 +1,9 @@
+(function($) {
+	$(document).ready(function() {
+		$.loading({
+			text : 'Carregando...',
+			overlay : '#23557E',
+			opacity: '60'
+		});
+	});
+})(jQuery);
---
+++ src/main/webapp/themes/dextra/index.js
@@ -0,0 +1,20 @@
+(function($) {
+	$(window).hashchange(function() {
+		if(!location.hash || location.hash == '#') {
+			location = '#home';
+			return;
+		}
+		if(!location.hash || location.hash == '#!') {
+			return;
+		}
+		var hash = location.hash.substring(1);
+		$.holy('./templates/' + hash + '.xml');
+	});
+
+	$(document).ready(function() {
+		$.holy('./templates/menu.xml');
+		$.holy('./templates/userbar.xml');
+		$('.message').messageMonitor();
+		$(window).hashchange();
+	});
+})(jQuery);
