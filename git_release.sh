bd4166ade1a284bda2526dd1a094825aaf6baeef
murer <fuweweu@gmail.com>
2012-04-09 10:46:31 -0300

    build

--- git_release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TAG="holy-$1"
-
-git tag -a "$TAG" -m "releasing $TAG"
-git push --tags
-
---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+VERSION=$1
+
+if [ "x$VERSION" == "x" ]; then
+	echo "release.sh <version>";
+	echo "";
+	exit 1;
+fi
+
+TAG="holy-$VERSION"
+
+echo "$VERSION" > WebContent/props/version.txt
+git commit -a "releasing $VERSION"
+git push
+
+git tag -a "$TAG" -m "releasing $TAG"
+git push --tags
+
+echo "master" > WebContent/props/version.txt
+git commit -a "releasing $VERSION"
+git push
+
+git pull
