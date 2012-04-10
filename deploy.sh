3013e09491711beb56e6b98044bca74463adf570
murer <fuweweu@gmail.com>
2012-04-10 11:02:18 -0300

    build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(echo "versions([" && git tag | grep "^holy\-2\.2\..\+$" | sed "s/^/'/g" | sed "s/$/',/g" && echo "'master']);") >  WebContent/props/all-versions.js
+(echo "versions([" && git tag | grep "^holy\-.\+$" | sed "s/^/'/g" | sed "s/$/',/g" && echo "'master']);") > WebContent/props/all-versions.js
 
 http_proxy=
 https_proxy=
