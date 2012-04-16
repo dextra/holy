ad0255e3e0a0fe8dbd84305673a7972573488703
murer <fuweweu@gmail.com>
2012-04-16 11:25:18 -0300

    minify

--- minify.sh
+++ minify.sh
@@ -11,19 +11,7 @@ echo "Total: js: $(echo "$FILES_JS" | wc -l), css: $(echo "$FILES_CSS" | wc -l)"
 done
 
 echo "index.min.html"
-INDEX_HTML=$(sed "s/\.\([jscss]\+\)\"/\.min\.\1\"/g" WebContent/index.html | sed "s/\.min\.min\./\.min\./g")
-
-echo "WebContent/min/holy.min.js"
-echo "$INDEX_HTML" | tail -n +2 | xpath -q -e "//head/script[@type='text/javascript']/@src" | cut -d"\"" -f2 | sed "s/^/WebContent\//g" | xargs cat | java -jar lib/yuicompressor-2.4.7.jar --type js --nomunge -o WebContent/min/holy.min.js
-
-echo "WebContent/min/holy.min.css"
-echo "$INDEX_HTML" | tail -n +2 | xpath -q -e "//head/link[@rel='stylesheet']/@href" | cut -d"\"" -f2 | sed "s/^/WebContent\//g" | xargs cat | java -jar lib/yuicompressor-2.4.7.jar --type css --nomunge -o WebContent/min/holy.min.css
-
-rm -rf WebContent/min/imgs
-echo "WebContent/imgs"
-find WebContent -name "imgs" | grep -v "WebContent/min/imgs" | while read k; do 
-	cp -R "$k" WebContent/min ; 
-done
+sed "s/\.\([jscss]\+\)\"/\.min\.\1\"/g" WebContent/index.html | sed "s/\.min\.min\./\.min\./g" > WebContent/index.min.html 
 
 echo "Minified"
 
