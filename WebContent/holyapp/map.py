209d63495e83413f778a2cfe071f2fffcb953ad2
murer <fuweweu@gmail.com>
2012-04-17 10:48:50 -0300

    pyu

---
+++ WebContent/holyapp/map.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+application = webapp.WSGIApplication(
+                                     [('/holyapp/helloworld', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
+    
